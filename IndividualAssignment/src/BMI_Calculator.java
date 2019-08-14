5586cbf7d9144b0377a15c30c4295ce976903866
xKhash <42858159+xKhash@users.noreply.github.com>
2019-08-14 19:00:24 +0800

    Update BMI_Calculator.java

--- IndividualAssignment/src/BMI_Calculator.java
+++ IndividualAssignment/src/BMI_Calculator.java
@@ -5,23 +5,27 @@ public class BMI_Calculator
 {
 	public static void main(String[] args) 
 	{
+		//Variables
 		String inchesString;
 		String poundsString;
 		double inches;
 		double pounds;
 		
+		//Clarification
 		JOptionPane.showMessageDialog(null,"Noted that this only works for people that are 20 years old and above");//information for user
 		
+		//Input
 		inchesString = JOptionPane.showInputDialog(null,"Please enter your height in inches?", "Height", JOptionPane.INFORMATION_MESSAGE);//prompt user
 		inches = Double.parseDouble(inchesString);//convert from string to double
 		poundsString = JOptionPane.showInputDialog(null,"Please enter your weight in pounds?", "Weight", JOptionPane.INFORMATION_MESSAGE);//prompt user
 		pounds = Double.parseDouble(poundsString);//convert from string to double
 		
-		//bmi only works with kg and cm
+		//Conversion(bmi only works with kg and cm)
 		double height = inches*0.0254;//convert inches to cm	
 		double weight = pounds*0.45359237;//convert pounds to kg
-		double BMI_Index = weight/(height*height);//calculate bmi
+		double BMI_Index = (double) Math.round((weight/(height*height)) * 100)/ 100;//calculate bmi(two decimal places)
 		
+		//Display
 		JOptionPane.showMessageDialog(null,"This is your BMI Index\n"
 				+ BMI_Index);//display user's bmi to them
 			//providing information to user about what user's bmi tells about their weight
