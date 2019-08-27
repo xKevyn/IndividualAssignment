092ed20d3d820bcc91b7e597e5232c7c319534ea
19026111 <19026111@WKS146.sunwayedu.local>
2019-08-27 10:10:11 +0800

    done

--- IndividualAssignment/src/AdvTicketPrice.java
+++ IndividualAssignment/src/AdvTicketPrice.java
@@ -8,6 +8,8 @@ import javax.swing.JLabel;
 
 public class AdvTicketPrice 
 {
+	static Font font = new Font("Comic Sans MS", Font.BOLD, 14);
+	
 	public static void main(String[] args) 
 	{
 		Display();
@@ -15,61 +17,60 @@ public class AdvTicketPrice
 	public static void Display()
 	{
 		//Variables
-				String AdvTicket_Day;
-				String[] day = {"Monday", "Tuesday","Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
-				int age;
+		String AdvTicket_Day;
+		String[] day = {"Monday", "Tuesday","Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+		int age;
+		
+		JLabel dayInput = new JLabel("Which day are you buying the ticket?");
+		dayInput.setFont(font);
+		//Background Color
+		UI();
+		
+		//Drop down list input for user to select
+		AdvTicket_Day = (String) JOptionPane.showInputDialog(null,dayInput,
+				"Advanced version of Ticket Price Program", JOptionPane.PLAIN_MESSAGE,null,day,day[0]);
+		
+		//Displaying different ticket price for each different day user selected and age they are
+		switch(AdvTicket_Day.toLowerCase())
+		{
+			//Monday to Thursday have the same price
+			case "monday":
+			case "tuesday":
+			case "wednesday":
+			case "thursday":
+				age = Age();
 				
-				JLabel dayInput = new JLabel("Which day are you buying the ticket?");
-				dayInput.setFont(new Font("Comic Sans MS", Font.BOLD, 14));
-				//Background Color
-				UI();
-				
-				//Drop down list input for user to select
-				AdvTicket_Day = (String) JOptionPane.showInputDialog(null,dayInput,
-						"Advanced version of Ticket Price Program", JOptionPane.PLAIN_MESSAGE,null,day,day[0]);
-				
-				//Displaying different ticket price for each different day user selected and age they are
-				switch(AdvTicket_Day.toLowerCase())
+				//Kids and elders are free
+				if (age <= 9 || age >= 66)
 				{
-					//Monday to Thursday have the same price
-					case "monday":
-					case "tuesday":
-					case "wednesday":
-					case "thursday":
-						age = Age();
-						
-						//Kids and elders are free
-						if (age <= 9 || age >= 66)
-						{
-							JOptionPane.showMessageDialog(null,"Your Ticket Price is FREE!!!");
-						}
-						else
-						{
-							JOptionPane.showMessageDialog(null,"Your ticket price is RM10");
-						}
-						break;
-					//Friday to Sunday have the same price
-					case "friday":
-					case "sunday":
-					case "saturday":
-						age = Age();
-						
-						//Kids and elders gets a discount of RM10
-						if (age <= 9 || age >= 66)
-						{
-							JOptionPane.showMessageDialog(null,"Your ticket price is RM10");
-						}
-						else
-						{
-							JOptionPane.showMessageDialog(null,"Your ticket price is RM20");
-						}
-						break;
-					default:
-						System.exit(0);
-						break;
+					JOptionPane.showMessageDialog(null,"Your Ticket Price is FREE!!!");
 				}
+				else
+				{
+					JOptionPane.showMessageDialog(null,"Your ticket price is RM10");
+				}
+				break;
+			//Friday to Sunday have the same price
+			case "friday":
+			case "sunday":
+			case "saturday":
+				age = Age();
+				
+				//Kids and elders gets a discount of RM10
+				if (age <= 9 || age >= 66)
+				{
+					JOptionPane.showMessageDialog(null,"Your ticket price is RM10");
+				}
+				else
+				{
+					JOptionPane.showMessageDialog(null,"Your ticket price is RM20");
+				}
+				break;
+			default:
+				System.exit(0);
+				break;
+		}
 	}
-	
 	//shorten the code
 	public static int Age()
 	{
@@ -78,7 +79,7 @@ public class AdvTicketPrice
 		
 		//Font change
 		JLabel ageInput = new JLabel("Please enter your age");
-		ageInput.setFont(new Font("Comic Sans MS", Font.BOLD, 14));
+		ageInput.setFont(font);
 		
 		ageString = JOptionPane.showInputDialog(null,ageInput, "Age", JOptionPane.PLAIN_MESSAGE);
 		age = Integer.parseInt(ageString);//convert from string to integer
